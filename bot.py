3773ad36f3b31ff553642cdebe0ecedac45e1bc9
Reajul999 <mdreajul999@gmail.com>
2026-05-26 22:36:33 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,42 +1,27 @@
 from telegram import Update
-from telegram.ext import (
-    Application,
-    CommandHandler,
-    MessageHandler,
-    ContextTypes,
-    filters,
-)
-
-from datetime import datetime
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 BOT_TOKEN = "6806483944:AAH9iZUSbha94raSegHC1SpZfH5UVrI4nrU"
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "Bot Active ✅"
-    )
+    await update.message.reply_text("Bot is running successfully ✅")
 
 
-async def time_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    current_time = datetime.now().strftime("%d-%m-%Y %I:%M:%S %p")
-    await update.message.reply_text(current_time)
+async def time(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    from datetime import datetime
 
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(update.message.text)
+    now = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    await update.message.reply_text(f"Current Date & Time:\n{now}")
 
 
 def main():
     app = Application.builder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("time", time_command))
-    app.add_handler(
-        MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
-    )
+    app.add_handler(CommandHandler("time", time))
 
-    print("Bot Running...")
+    print("Bot running...")
     app.run_polling()
 
 
